7deb351123fc23ac35365f2d884581365be6245a
ashwindurairaj <ashwindurairaj22@gmail.com>
2025-08-28 07:16:31 +0530

    [Ashwin] pushing to main

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+
+
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+
+    SharedModule // ✅ Import this so app-header and app-sidebar are usable
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
